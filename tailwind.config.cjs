446a70db1984eba1eed75e20cde6cba3b6e7c293
Alex Marshall <alex.k.marshall83@gmail.com>
2022-07-14 08:45:50 +0100

    setup fonts

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,12 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: ["'IBM Plex Sans'", ...defaultTheme.fontFamily.sans],
+    },
   },
   plugins: [],
 };
